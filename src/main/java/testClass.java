70750dd08023756ac13d2bb18d06c25d0e84b746
DaniloBez <Danylo.bezukh@gmail.com>
2025-03-25 17:27:27 +0200

    small test

---
+++ src/main/java/testClass.java
@@ -0,0 +1,5 @@
+public class testClass {
+    public boolean test(int i){
+        return i % 2 == 0;
+    }
+}
---
+++ src/test/java/testTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class testClassTest {
+
+    @Test
+    void test1() {
+        testClass t = new testClass();
+        assertTrue(t.test(2));
+        assertTrue(t.test(4));
+    }
+}
