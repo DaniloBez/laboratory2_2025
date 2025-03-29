632e270782a0257c8ea60851d4856a9ec6952f1a
DaniloBez <Danylo.bezukh@gmail.com>
2025-03-29 19:54:07 +0200

    fix!: jacoco fix

---
+++ src/test/java/FirstClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirstClassTest {
+    @Test
+    void test() {
+        FirstClass firstClass = new FirstClass();
+    }
+}
